a047942b107d742985874ab9a49e384f56ca6700
Wendell Marcos <wendell.marcos.code@gmail.com>
2023-10-24 17:21:34 +0100

    Iniciando Projeto Agendamento de Consultas, telas criadas

---
+++ src/nutrisoftconsultas/NutriSoftConsultas.java
@@ -0,0 +1,17 @@
+
+package nutrisoftconsultas;
+
+/**
+ *
+ * @author Wendell Marcos
+ */
+public class NutriSoftConsultas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
